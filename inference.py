1d816041cb4e25e94e745a1227db023b3aa3482f
njanirudh <anijaya9@gmail.com>
2020-12-18 00:30:04 +0100

    Inference with model working

---
+++ inference.py
@@ -0,0 +1,59 @@
+# https://gist.github.com/AFAgarap/4f8a8d8edf352271fa06d85ba0361f26
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg',warn=False, force=True)
+
+from src.autoencoders.basic_autoencoder import AutoEncoder
+from src.dataset.vm_dataset import VisuomotorDataset
+
+MODEL_SAVE = "/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/" \
+             "AutoEncoders/model/prototype.pth"
+
+
+model = AutoEncoder(input_shape=784,output_shape=16)
+model.load_state_dict(torch.load(MODEL_SAVE))
+model.eval()
+
+transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+
+DATASET_PATH = "/home/anirudh/Desktop/main_dataset/door_1/*.png"
+test_dataset = VisuomotorDataset(DATASET_PATH,transform,(28,28))
+
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=10, shuffle=False
+)
+
+test_examples = None
+
+with torch.no_grad():
+    for batch_features in test_loader:
+        batch_features = batch_features[0]
+        test_examples = batch_features.view(-1, 784)
+        reconstruction = model(test_examples)
+        break
+
+with torch.no_grad():
+    number = 10
+    plt.figure(figsize=(20, 4))
+    for index in range(number):
+        # display original
+        ax = plt.subplot(2, number, index + 1)
+        plt.imshow(test_examples[index].numpy().reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # display reconstruction
+        ax = plt.subplot(2, number, index + 1 + number)
+        plt.imshow(reconstruction[index].numpy().reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
--- training.py
+++ training.py
@@ -13,15 +13,17 @@ seed = 42
 torch.manual_seed(seed)
 
 batch_size = 512
-epochs = 5
+epochs = 100
 learning_rate = 1e-3
+
+DATASET_PATH = "/home/anirudh/Desktop/main_dataset/door_1/*.png"
+MODEL_SAVE_PATH = "/home/anirudh/HBRS/Master-Thesis/NJ-2020-thesis/AutoEncoders/model/prototype.pth"
 
 transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
 
 # train_dataset = torchvision.datasets.MNIST(
 #     root="~/torch_datasets", train=True, transform=transform, download=True
 # )
-DATASET_PATH = "/home/anirudh/Desktop/main_dataset/door_1/*.png"
 train_dataset = VisuomotorDataset(DATASET_PATH,transform,(28,28))
 
 train_loader = torch.utils.data.DataLoader(
@@ -74,3 +76,16 @@ for epoch in range(epochs):
 
     # display the epoch training loss
     print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, epochs, loss))
+
+
+# Print model's state_dict
+print("Model's state_dict:")
+for param_tensor in model.state_dict():
+    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+
+# Print optimizer's state_dict
+print("Optimizer's state_dict:")
+for var_name in optimizer.state_dict():
+    print(var_name, "\t", optimizer.state_dict()[var_name])
+
+torch.save(model.state_dict(), MODEL_SAVE_PATH)
